9052fca472ae103c692844bf585fdabfb014b25a
Bela Boros <bela.boros@gmail.com>
2020-01-20 22:44:13 +0100

    added rancher server

---
+++ rancher_server/10_start_rancher.sh
@@ -0,0 +1,8 @@
+# latest Rancher version
+#sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+
+
+# a specific Rancher version
+sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher:v2.3.3
+
+
