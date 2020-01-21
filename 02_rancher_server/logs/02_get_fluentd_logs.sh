d311de5d4eb5748e4d39f50caf02b58cca37b9f9
Bela Boros <Bela.Boros@ah.nl>
2020-01-21 12:56:02 +0100

    Generic query of FluentD logs

--- 02_rancher_server/logs/02_get_fluentd_logs.sh
+++ 02_rancher_server/logs/02_get_fluentd_logs.sh
@@ -1,10 +1,47 @@
-rm -rf *.log
+#!/bin/bash
+# Query the log files of logging components
 
 
-POD=rancher-logging-fluentd-linux-ll2r5
+#rm -rf *.log
+#POD=rancher-logging-fluentd-linux-ll2r5
+#kubectl logs -n cattle-logging pod/${POD} -c rancher-logging-fluentd > rancher-logging-fluentd.log
+#kubectl logs -n cattle-logging pod/${POD} -c rancher-logging-fluentd-reloader > rancher-logging-fluentd-reloader.log
 
 
-kubectl logs -n cattle-logging pod/${POD} -c rancher-logging-fluentd > rancher-logging-fluentd.log
-kubectl logs -n cattle-logging pod/${POD} -c rancher-logging-fluentd-reloader > rancher-logging-fluentd-reloader.log
+
+
+
+#kubectl get pods -A | grep rancher-logging-fluentd-linux
+#kubectl get pods -o custom-columns=POD_NAME:.metadata.name -A | grep fluentd
+
+rm *.log
+
+
+PODS=$(kubectl get pods -o custom-columns=POD_NAME:.metadata.name -A | grep fluentd)
+#echo -e "FluentD PODS:\n${PODS}"
+
+for POD in ${PODS}; do
+    echo -e "\n\n${POD}_rancher-logging-fluentd"
+    kubectl -n cattle-logging logs ${POD} -c rancher-logging-fluentd > ${POD}_rancher-logging-fluentd.log
+
+    echo -e "\n\n${POD}_rancher-logging-fluentd-reloader"
+    kubectl -n cattle-logging logs ${POD} -c rancher-logging-fluentd-reloader > ${POD}_rancher-logging-fluentd-reloader.log
+done
+
+
+# save the config JSON file
+kubectl get secret -n cattle-logging rancher-logging-fluentd -o jsonpath='{.data.cluster\.conf}' | base64 --decode >rancher-logging-fluentd.config.json
+
+PODS=$(kubectl get pods -o custom-columns=POD_NAME:.metadata.name -A | grep rancher-logging-log-aggregator)
+echo -e "\n\nLog aggregator PODs:"
+for POD in ${PODS}; do
+    echo -e "\n\n${POD}"
+    kubectl -n cattle-logging logs ${POD} -c log-aggregator > ${POD}.log
+    #kubectl -n cattle-logging get pods ${POD}
+
+done
+
+
+
 
 
